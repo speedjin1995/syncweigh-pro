92d74b53ca1e1ee4e3c085e034022ccd92625c26
speedjin1995 <speedjin1995@gmail.com>
2023-11-06 23:22:06 +0800

    fix config

--- layouts/config.php
+++ layouts/config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'sql909.main-hosting.eu');
-define('DB_USERNAME', 'u782565293_sync');
+define('DB_SERVER', 'srv625.hstgr.io');
+define('DB_USERNAME', 'u664110560_sync');
 define('DB_PASSWORD', 'Aa@111222333');
-define('DB_NAME', 'u782565293_sync');
+define('DB_NAME', 'u664110560_sync');
 
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
