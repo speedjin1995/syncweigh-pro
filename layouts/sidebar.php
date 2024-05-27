529260298a7848bd45ffbdd4326079e20566cfbe
speedjin1995 <speedjin1995@gmail.com>
2024-05-27 23:54:10 +0800

    hide billboard

--- layouts/sidebar.php
+++ layouts/sidebar.php
@@ -33,9 +33,9 @@
             </div>
             <ul class="navbar-nav" id="navbar-nav">
                 <li class="menu-title"><span><?=$lang['t-menu']?></span></li>
-                <li class="nav-item">
+                <!--li class="nav-item">
                     <a href="index.php" class="nav-link"><i class="mdi mdi-billboard"></i><?=$lang['t-billboard']?></a>
-                </li>
+                </li-->
                 <li class="nav-item">
                     <a href="weighing.php" class="nav-link"><i class="mdi mdi-weight"></i><?=$lang['t-weighing']?></a>
                 </li>                
