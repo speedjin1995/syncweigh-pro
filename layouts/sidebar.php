bc319834535eaf3588803b06d0996a67002afbb5
JihB_0105 <ngjihbin1234@gmail.com>
2025-02-20 21:49:45 +0800

    make logo bigger

--- layouts/sidebar.php
+++ layouts/sidebar.php
@@ -8,7 +8,7 @@
                 <img src="assets/images/logo-sm.png" alt="" height="22">
             </span>
             <span class="logo-lg">
-                <img src="assets/images/logo-lg.png" alt="" height="17">
+                <img src="assets/images/logo-lg.png" alt="" height="50">
             </span>
         </a>
         <!-- Light Logo-->
@@ -17,7 +17,7 @@
                 <img src="assets/images/logo-sm.png" alt="" height="22">
             </span>
             <span class="logo-lg">
-                <img src="assets/images/logo-lg.png" alt="" height="17">
+                <img src="assets/images/logo-lg.png" alt="" height="50">
             </span>
         </a>
         <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover"
