a2db96a131af503aec13df84e4364b6c8bdf84c8
TimMingHey <nblim2018@gmail.com>
2025-03-12 11:11:51 +0800

    Update DB Config

--- layouts/config.php
+++ layouts/config.php
@@ -2,9 +2,9 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'srv597.hstgr.io');
-define('DB_USERNAME', 'u664110560_sync');
-define('DB_PASSWORD', 'Aa@111222333');
-define('DB_NAME', 'u664110560_sync');
+define('DB_USERNAME', 'u664110560_treer');
+define('DB_PASSWORD', '@‌Sync5500');
+define('DB_NAME', 'u664110560_treer');
 
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
--- php/db_connect.php
+++ php/db_connect.php
@@ -1,6 +1,6 @@
 <?php
 date_default_timezone_set('Asia/Kuala_Lumpur');
-$db = mysqli_connect("srv597.hstgr.io", "u664110560_sync", "Aa@111222333", "u664110560_sync");
+$db = mysqli_connect("srv597.hstgr.io", "u664110560_treer", "@‌Sync5500", "u664110560_treer");
 
 if(mysqli_connect_errno()){
     echo 'Database connection failed with following errors: ' . mysqli_connect_error();
