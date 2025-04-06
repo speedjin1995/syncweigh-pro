6fe05f10f6f70627cd5d8f1ca480602321a05380
TimMingHey <nblim2018@gmail.com>
2025-04-06 16:32:45 +0800

    Update weighingReport.php

--- weighingReport.php
+++ weighingReport.php
@@ -183,13 +183,13 @@ $groupby = array(
                             </div>
                             
                             <div class="row">
-                                <div class="col-xl-4 col-md-6">
+                                <div class="col-xl-3 col-md-6">
                                     <!-- card -->
-                                    <div class="card card-animate">
+                                    <div class="card card-animate card-success">
                                         <div class="card-body">
                                             <div class="d-flex align-items-center">
                                                 <div class="flex-grow-1 overflow-hidden">
-                                                    <p class="text-uppercase fw-medium text-muted text-truncate mb-0">
+                                                    <p class="text-uppercase fw-medium text-truncate mb-0" style="color: black;">
                                                         Sales</p>
                                                 </div>
                                             </div>
@@ -200,8 +200,8 @@ $groupby = array(
                                                     </h4>
                                                 </div>
                                                 <div class="avatar-sm flex-shrink-0">
-                                                    <span class="avatar-title bg-soft-success rounded fs-3">
-                                                        <i class="bx bx-dollar-circle text-success"></i>
+                                                    <span class="avatar-title bg-soft-danger rounded fs-3">
+                                                        <i class="bx bx-dollar-circle text-danger"></i>
                                                     </span>
                                                 </div>
                                             </div>
@@ -209,13 +209,13 @@ $groupby = array(
                                     </div><!-- end card -->
                                 </div><!-- end col -->
 
-                                <div class="col-xl-4 col-md-6">
+                                <div class="col-xl-3 col-md-6">
                                     <!-- card -->
-                                    <div class="card card-animate">
+                                    <div class="card card-animate card-warning">
                                         <div class="card-body">
                                             <div class="d-flex align-items-center">
                                                 <div class="flex-grow-1 overflow-hidden">
-                                                    <p class="text-uppercase fw-medium text-muted text-truncate mb-0">
+                                                    <p class="text-uppercase fw-medium text-truncate mb-0" style="color: black;">
                                                         Purchase</p>
                                                 </div>
                                             </div>
@@ -234,13 +234,13 @@ $groupby = array(
                                     </div><!-- end card -->
                                 </div><!-- end col -->
 
-                                <div class="col-xl-4 col-md-6">
+                                <div class="col-xl-3 col-md-6">
                                     <!-- card -->
-                                    <div class="card card-animate">
+                                    <div class="card card-animate card-info">
                                         <div class="card-body">
                                             <div class="d-flex align-items-center">
                                                 <div class="flex-grow-1 overflow-hidden">
-                                                    <p class="text-uppercase fw-medium text-muted text-truncate mb-0">
+                                                    <p class="text-uppercase fw-medium text-truncate mb-0" style="color: black;">
                                                     Local</p>
                                                 </div>
                                             </div>
@@ -253,6 +253,32 @@ $groupby = array(
                                                 <div class="avatar-sm flex-shrink-0">
                                                     <span class="avatar-title bg-soft-warning rounded fs-3">
                                                         <i class="bx bx-user-circle text-warning"></i>
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        </div><!-- end card body -->
+                                    </div><!-- end card -->
+                                </div><!-- end col -->
+
+                                <div class="col-xl-3 col-md-6">
+                                    <!-- card -->
+                                    <div class="card card-animate card-danger">
+                                        <div class="card-body">
+                                            <div class="d-flex align-items-center">
+                                                <div class="flex-grow-1 overflow-hidden">
+                                                    <p class="text-uppercase fw-medium text-truncate mb-0" style="color: black;">
+                                                    Misc</p>
+                                                </div>
+                                            </div>
+                                            <div class="d-flex align-items-end justify-content-between mt-4">
+                                                <div>
+                                                    <h4 class="fs-22 fw-semibold ff-secondary mb-4"><span
+                                                            class="counter-value" id="miscInfo">0</span>
+                                                    </h4>
+                                                </div>
+                                                <div class="avatar-sm flex-shrink-0">
+                                                    <span class="avatar-title bg-soft-warning rounded fs-3">
+                                                        <i class="bx bx-ruler text-warning"></i>
                                                     </span>
                                                 </div>
                                             </div>
