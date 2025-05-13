8b3e82316a0fef8d6c8545527cfb7697f0ee8d03
speedjin1995 <speedjin1995@gmail.com>
2025-05-13 19:29:41 +0800

    Update db_connect.php

--- php/db_connect.php
+++ php/db_connect.php
@@ -1,6 +1,6 @@
 <?php
 date_default_timezone_set('Asia/Kuala_Lumpur');
-$db = mysqli_connect("srv597.hstgr.io", "u664110560_xs_uat", "@Sync5500", "u664110560_xs_uat");
+$db = mysqli_connect("localhost", "u664110560_hungli", "@Sync5500", "u664110560_hungli");
 
 if(mysqli_connect_errno()){
     echo 'Database connection failed with following errors: ' . mysqli_connect_error();
