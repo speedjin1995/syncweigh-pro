71d031d60815d98e46a59e2c8bb225f53239b3a7
speedjin1995 <speedjin1995@gmail.com>
2025-05-13 19:29:18 +0800

    Update config.php

--- layouts/config.php
+++ layouts/config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'srv597.hstgr.io');
-define('DB_USERNAME', 'u664110560_xs_uat');
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'u664110560_hungli');
 define('DB_PASSWORD', '@Sync5500');
-define('DB_NAME', 'u664110560_xs_uat');
+define('DB_NAME', 'u664110560_hungli');
 
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
