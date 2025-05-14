fd9828874b471044cc6d30fcb5cdfd1d9a6806ac
JihB_0105 <ngjihbin1234@gmail.com>
2025-05-14 21:09:15 +0800

    Completed Supplier enhancement

--- supplier.php
+++ supplier.php
@@ -125,7 +125,19 @@
                                                                                 <div class="row">
                                                                                     <label for="companyRegNo" class="col-sm-4 col-form-label">Company Reg No</label>
                                                                                     <div class="col-sm-8">
-                                                                                        <input type="text" class="form-control" id="companyRegNo" name="companyRegNo" placeholder="Company Reg No">
+                                                                                        <div class="row">
+                                                                                            <div class="col-sm-4">
+                                                                                                <input type="text" class="form-control" id="companyRegNo" name="companyRegNo">
+                                                                                            </div>
+                                                                                            <div class="col-sm-8">
+                                                                                                <div class="row">
+                                                                                                    <label for="newRegNo" class="col-sm-4 col-form-label">New Reg No</label>
+                                                                                                    <div class="col-sm-8">
+                                                                                                        <input type="text" class="form-control" id="newRegNo" name="newRegNo" required>
+                                                                                                    </div>
+                                                                                                </div>
+                                                                                            </div>
+                                                                                        </div>
                                                                                     </div>
                                                                                 </div>
                                                                             </div>
@@ -161,14 +173,14 @@
                                                                                     </div>
                                                                                 </div>
                                                                             </div>
-                                                                            <div class="col-xxl-12 col-lg-12 mb-3">
+                                                                            <!-- <div class="col-xxl-12 col-lg-12 mb-3">
                                                                                 <div class="row">
                                                                                     <label for="addressLine4" class="col-sm-4 col-form-label">Address Line 4</label>
                                                                                     <div class="col-sm-8">
                                                                                         <input type="text" class="form-control" id="addressLine4" name="addressLine4" placeholder="Address Line 4">
                                                                                     </div>
                                                                                 </div>
-                                                                            </div>
+                                                                            </div> -->
                                                                             <div class="col-xxl-12 col-lg-12 mb-3">
                                                                                 <div class="row">
                                                                                     <label for="phoneNo" class="col-sm-4 col-form-label">Phone No</label>
@@ -182,6 +194,30 @@
                                                                                     <label for="faxNo" class="col-sm-4 col-form-label">Fax No</label>
                                                                                     <div class="col-sm-8">
                                                                                         <input type="text" class="form-control" id="faxNo" name="faxNo" placeholder="Fax No">
+                                                                                    </div>
+                                                                                </div>
+                                                                            </div>
+                                                                            <div class="col-xxl-12 col-lg-12 mb-3">
+                                                                                <div class="row">
+                                                                                    <label for="contactName" class="col-sm-4 col-form-label">Contact Name</label>
+                                                                                    <div class="col-sm-8">
+                                                                                        <input type="text" class="form-control" id="contactName" name="contactName" placeholder="Contact Name">
+                                                                                    </div>
+                                                                                </div>
+                                                                            </div>
+                                                                            <div class="col-xxl-12 col-lg-12 mb-3">
+                                                                                <div class="row">
+                                                                                    <label for="icNo" class="col-sm-4 col-form-label">I/C No</label>
+                                                                                    <div class="col-sm-8">
+                                                                                        <input type="text" class="form-control" id="icNo" name="icNo" placeholder="I/C No">
+                                                                                    </div>
+                                                                                </div>
+                                                                            </div>
+                                                                            <div class="col-xxl-12 col-lg-12 mb-3">
+                                                                                <div class="row">
+                                                                                    <label for="tinNo" class="col-sm-4 col-form-label">Tin No</label>
+                                                                                    <div class="col-sm-8">
+                                                                                        <input type="text" class="form-control" id="tinNo" name="tinNo" placeholder="Tin No">
                                                                                     </div>
                                                                                 </div>
                                                                             </div>
@@ -226,6 +262,23 @@
                                             </div>
                                         </div>
                                     </div>
+                                    <div class="modal fade" id="errorModal" style="display:none">
+                                        <div class="modal-dialog modal-xl" style="max-width: 50%;">
+                                            <div class="modal-content">
+                                                <div class="modal-header bg-gray-dark color-palette">
+                                                    <h4 class="modal-title">Error Log</h4>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <div class="row">
+                                                        <div class="form-group">
+                                                            <ol id="errorList" class="text-danger mt-2" style="padding-left: 20px;"></ol>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>   
                                 </div>
                             </div> <!-- end row-->
 
@@ -254,7 +307,7 @@
                                                                 </button>
                                                                 <button type="button" id="multiDeactivate" class="btn btn-warning waves-effect waves-light">
                                                                     <i class="fa-solid fa-ban align-middle me-1"></i>
-                                                                    Delete Raw Material
+                                                                    Delete Supplier
                                                                 </button>
                                                                 <button type="button" id="addSupplier" class="btn btn-success waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#addModal">
                                                                     <i class="ri-add-circle-line align-middle me-1"></i>
@@ -270,12 +323,16 @@
                                                                     <th><input type="checkbox" id="selectAllCheckbox" class="selectAllCheckbox"></th>
                                                                     <th>Supplier Code</th>
                                                                     <th>Company Reg No</th>
+                                                                    <th>New Reg No</th>
                                                                     <th>Company Name</th>
                                                                     <th>Address Line 1</th>
                                                                     <th>Address Line 2</th>
                                                                     <th>Address Line 3</th>
                                                                     <th>Phone No</th>
                                                                     <th>Fax No</th>
+                                                                    <th>Contact Name</th>
+                                                                    <th>I/C No</th>
+                                                                    <th>Tin No</th>
                                                                     <th>Status</th>
                                                                     <th>Action</th>
                                                                 </tr>
@@ -361,12 +418,16 @@ $(function () {
             },
             { data: 'supplier_code' },
             { data: 'company_reg_no' },
+            { data: 'new_reg_no' },
             { data: 'name' },
             { data: 'address_line_1' },
             { data: 'address_line_2' },
             { data: 'address_line_3' },
             { data: 'phone_no' },
             { data: 'fax_no' },
+            { data: 'contact_name' },
+            { data: 'ic_no' },
+            { data: 'tin_no' },
             { 
                 data: 'id',
                 render: function ( data, type, row ) {
@@ -456,6 +517,19 @@ $(function () {
                     $("#failBtn").attr('data-toast-text', obj.message );
                     $("#failBtn").click();
                 } 
+                else if (obj.status === 'error') {
+                    $('#spinnerLoading').hide();
+                    $('#uploadModal').modal('hide');
+                    // alert(obj.message);
+                    // $("#failBtn").attr('data-toast-text', obj.message );
+                    // $("#failBtn").click();
+                    $('#errorModal').find('#errorList').empty();
+                    var errorMessage = obj.message;
+                    for (var i = 0; i < errorMessage.length; i++) {
+                        $('#errorModal').find('#errorList').append(`<li>${errorMessage[i]}</li>`);                            
+                    }
+                    $('#errorModal').modal('show');
+                } 
                 else {
                     $('#spinnerLoading').hide();
                     $("#failBtn").attr('data-toast-text', 'Failed to save');
@@ -470,12 +544,15 @@ $(function () {
         $('#addModal').find('#supplierCode').val("");
         $('#addModal').find('#companyName').val("");
         $('#addModal').find('#companyRegNo').val("");
+        $('#addModal').find('#newRegNo').val("");
         $('#addModal').find('#addressLine1').val("");
         $('#addModal').find('#addressLine2').val("");
         $('#addModal').find('#addressLine3').val("");
-        $('#addModal').find('#addressLine4').val("");
         $('#addModal').find('#phoneNo').val("");
         $('#addModal').find('#faxNo').val("");
+        $('#addModal').find('#contactName').val("");
+        $('#addModal').find('#icNo').val("");
+        $('#addModal').find('#tinNo').val("");
 
         // Remove Validation Error Message
         $('#addModal .is-invalid').removeClass('is-invalid');
@@ -599,7 +676,7 @@ function displayPreview(data) {
     var headers = jsonData[0];
 
     // Ensure we handle cases where there may be less than 15 columns
-    while (headers.length < 9) {
+    while (headers.length < 12) {
         headers.push(''); // Adding empty headers to reach 15 columns
     }
 
@@ -616,11 +693,11 @@ function displayPreview(data) {
         var rowData = jsonData[i];
 
         // Ensure we handle cases where there may be less than 15 cells in a row
-        while (rowData.length < 9) {
+        while (rowData.length < 12) {
             rowData.push(''); // Adding empty cells to reach 15 columns
         }
 
-        for (var j = 0; j < 9; j++) {
+        for (var j = 0; j < 12; j++) {
             var cellData = rowData[j];
             var formattedData = cellData;
 
@@ -650,12 +727,15 @@ function edit(id){
             $('#addModal').find('#supplierCode').val(obj.message.supplier_code);
             $('#addModal').find('#companyName').val(obj.message.name);
             $('#addModal').find('#companyRegNo').val(obj.message.company_reg_no);
+            $('#addModal').find('#newRegNo').val(obj.message.new_reg_no);
             $('#addModal').find('#addressLine1').val(obj.message.address_line_1);
             $('#addModal').find('#addressLine2').val(obj.message.address_line_2);
             $('#addModal').find('#addressLine3').val(obj.message.address_line_3);
-            $('#addModal').find('#addressLine4').val(obj.message.address_line_4);
             $('#addModal').find('#phoneNo').val(obj.message.phone_no);
             $('#addModal').find('#faxNo').val(obj.message.fax_no);
+            $('#addModal').find('#contactName').val(obj.message.contact_name);
+            $('#addModal').find('#icNo').val(obj.message.ic_no);
+            $('#addModal').find('#tinNo').val(obj.message.tin_no);
 
             // Remove Validation Error Message
             $('#addModal .is-invalid').removeClass('is-invalid');
