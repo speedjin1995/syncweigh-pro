fc3f7ecc2e079ad5196f8cd0bb74a6e3eae6f261
JihB_0105 <ngjihbin1234@gmail.com>
2025-06-10 21:09:13 +0800

    pushed for GR export

--- php/exportDoGr.php
+++ php/exportDoGr.php
@@ -194,7 +194,7 @@ if ($isMulti == 'N'){
                         $select_stmt->close();
                     }
 
-                    $lineData = array($poNo, $row2['transaction_id'], $tareDateTime, $row2['lorry_plate_no1'], $row2['supplier_code'], $row2['supplier_name'], $row2['raw_mat_code'], $row2['raw_mat_name'], $row2['destination'], $row2['transporter_code'], $exDel, $orderNo, $row2['delivery_no'], $qty, $uom, $row2['plant_code'], $row2['plant_code'], $unitPrice, $amt);
+                    $lineData = array($poNo, $row2['transaction_id'], $tareDateTime, $row2['lorry_plate_no1'], $row2['supplier_code'], $row2['supplier_name'], $row2['raw_mat_code'], $row2['raw_mat_name'], $row2['destination'], $row2['transporter_code'], $exDel, '', $row2['delivery_no'], $qty, $uom, $row2['plant_code'], $row2['plant_code'], $unitPrice, $amt);
 
                     # Added checking to fix duplicated issue
                     if (!empty($lineData)) {
@@ -282,20 +282,63 @@ if ($isMulti == 'N'){
         $fileName = "GR-data_" . date('Y-m-d') . ".xls";
 
         // Fetch records from database
-        $query = $db->query("select * from Purchase_Order WHERE deleted = '0'".$searchQuery);
+        $query = "select * from Weight where id IN (". $id .") order by id asc";
 
-        if($query->num_rows > 0){ 
+        $do_stmt = $db->query($query);
+        if($do_stmt->num_rows > 0){  
             // Output each row of the data 
-            while($row = $query->fetch_assoc()){ 
-                $lineData = []; // Ensure it starts as an empty array each iteration
-                $lineData = array($row['company_code'], $row['company_name'], $row['supplier_code'], $row['supplier_name'], $row['plant_code'], $row['plant_name'], $row['raw_mat_code'], $row['raw_mat_name'], $row['po_no'], $row['order_date'], $row['exquarry_or_delivered'], $row['balance']);
+            while($row = $do_stmt->fetch_assoc()){
+                $poNo = $row['purchase_order']; 
+                $fromDate = DateTime::createFromFormat('d-m-Y H:i', $_GET['fromDate']);
+                $fromDateTime = $fromDate->format('Y-m-d H:i:00');
+                $toDate = DateTime::createFromFormat('d-m-Y H:i', $_GET['toDate']);
+                $toDateTime = $toDate->format('Y-m-d H:i:59');
 
-                # Added checking to fix duplicated issue
-                if (!empty($lineData)) {
-                    array_walk($lineData, 'filterData'); 
-                    $excelData .= implode("\t", array_values($lineData)) . "\n"; 
+                $doQuery = "select * from Weight WHERE purchase_order = '$poNo' AND tare_weight1_date >= '$fromDateTime' AND tare_weight1_date <= '$toDateTime' AND is_complete = 'Y' AND status = '0'";
+                $doRecords = mysqli_query($db, $doQuery);
+                $weighingData = array();
+
+                while($row2 = mysqli_fetch_assoc($doRecords)) {
+                    $lineData = []; // Ensure it starts as an empty array each iteration
+                    $tareDate = DateTime::createFromFormat('Y-m-d H:i:s', $row2['tare_weight1_date']);
+                    $tareDateTime = $tareDate->format('d/m/Y');
+                    $exDel = ($row2['ex_del'] == 'EX') ? 'E' : 'D';
+
+                    $uom = '';
+                    $qty = '';
+                    $amt = '';
+                    if ($select_stmt = $db->prepare("SELECT * FROM Purchase_Order WHERE po_no=? AND raw_mat_code=? AND plant_code=? AND deleted='0'")) {
+                        $select_stmt->bind_param('sss', $poNo, $row2['raw_mat_code'], $row2['plant_code']);
+                        $select_stmt->execute();
+                        $result = $select_stmt->get_result();
+                        if ($row3 = $result->fetch_assoc()) { 
+                            $uom = searchUnitById($row3['converted_unit'], $db);
+                            $rawMatId = searchRawMatIdByCode($row3['raw_mat_code'], $db);
+                            $unitPrice = $row3['unit_price'];
+
+                            if ($update_stmt = $db->prepare("SELECT * FROM Raw_Mat_UOM WHERE raw_mat_id=? AND unit_id='2' AND status='0'")) {
+                                $update_stmt->bind_param('s', $rawMatId);
+                                $update_stmt->execute();
+                                $result2 = $update_stmt->get_result();
+                                if ($row4 = $result2->fetch_assoc()) {
+                                    $qty = $row2['nett_weight1'] * $row4['rate'];
+                                    $amt = $qty * $unitPrice;
+                                }
+                                $update_stmt->close();
+                            }
+                        }
+                        $select_stmt->close();
+                    }
+
+                    $lineData = array($poNo, $row2['transaction_id'], $tareDateTime, $row2['lorry_plate_no1'], $row2['supplier_code'], $row2['supplier_name'], $row2['raw_mat_code'], $row2['raw_mat_name'], $row2['destination'], $row2['transporter_code'], $exDel, '', $row2['delivery_no'], $qty, $uom, $row2['plant_code'], $row2['plant_code'], $unitPrice, $amt);
+
+                    # Added checking to fix duplicated issue
+                    if (!empty($lineData)) {
+                        array_walk($lineData, 'filterData'); 
+                        $excelData .= implode("\t", array_values($lineData)) . "\n"; 
+                    }
                 }
-            } 
+            }
         }else{ 
             $excelData .= 'No records found...'. "\n"; 
         } 
