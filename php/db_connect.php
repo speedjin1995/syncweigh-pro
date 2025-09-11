27e265088cf0927a68120a676291b1cad52bab6a
Sky Ng <sky.ng@dcap.my>
2025-09-11 10:55:33 +0800

    close error reporting

--- php/db_connect.php
+++ php/db_connect.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(0);
+ini_set('display_errors', 0);
 date_default_timezone_set('Asia/Kuala_Lumpur');
 $db = mysqli_connect("srv597.hstgr.io", "u664110560_bioeneco", "@Sync5500", "u664110560_bioeneco");
 
