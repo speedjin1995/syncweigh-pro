3be68228dc9f4273eee0a21299891bb26da2e7fd
Ng Jih Bin <sky.ng@dcap.my>
2025-12-21 13:27:44 +0800

    hide bitumen pg 76 first

--- assetManagement.php
+++ assetManagement.php
@@ -161,7 +161,7 @@ else{
                                                                                             <option value="Bitumen">Bitumen</option>
                                                                                             <option value="LFO">LFO</option>
                                                                                             <option value="Diesel">Diesel</option>
-                                                                                            <option value="PG 76">Bitumen PG 76</option>
+                                                                                            <!-- <option value="PG 76">Bitumen PG 76</option> -->
                                                                                         </select>
                                                                                     </div>
                                                                                 </div>
