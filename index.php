cb5e3113cdb09db676d249cb23018e214e5281b3
Ng Jih Bin <sky.ng@dcap.my>
2026-04-18 13:03:32 +0800

    bring back changes from prod

--- index.php
+++ index.php
@@ -1871,7 +1871,7 @@ if (hasPermission('Weighing', ['view_all_plants'])){
                             </div>`;
                         }
 
-                        if (isSADMIN || (ermissions['Weighing'] && permissions['Weighing'][transactionKey] && permissions['Weighing'][transactionKey].includes('delete'))) {
+                        if (isSADMIN || (permissions['Weighing'] && permissions['Weighing'][transactionKey] && permissions['Weighing'][transactionKey].includes('delete'))) {
                             buttons += `
                             <div class="col-auto">
                                 <button title="Delete" type="button" id="delete${data}" onclick="deactivate(${data})" class="btn btn-danger btn-sm">
